cf7dea84921277c6cad3f3ca2572195e3c598155
Radiant-F <radiantfadilah0@gmail.com>
2023-09-01 15:28:29 +0700

    testing slice

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import RNBootSplash from 'react-native-bootsplash';
+import SignIn from './src/screens/SignIn';
 
 export default function App() {
   RNBootSplash.hide({fade: true});
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
+  return <SignIn />;
 }
 
 const styles = StyleSheet.create({});
