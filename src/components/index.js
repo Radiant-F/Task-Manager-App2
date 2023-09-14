ce09551fe0c9ffb329e6c08bd24a8df58d4504ee
Radiant-F <radiantfadilah0@gmail.com>
2023-09-14 21:43:49 +0700

    update import component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Background from './screens/Background';
 import Gap from './screens/Gap';
 import UserData from './screens/UserData';
+import RenderItem from './screens/home/RenderItem';
+import ModalAddTask from './screens/home/ModalAddTask';
+import ModalEditTask from './screens/home/ModalEditTask';
 
-export {Background, Gap, UserData};
+export {Background, Gap, UserData, RenderItem, ModalAddTask, ModalEditTask};
